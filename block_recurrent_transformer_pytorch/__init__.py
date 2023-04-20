86be0c0d3316fb070f9fadcc567a2664fbacf690
Phil Wang <lucidrains@gmail.com>
2023-04-20 14:10:58 -0700

    update to einops 0.6.1 or greater

--- block_recurrent_transformer_pytorch/__init__.py
+++ block_recurrent_transformer_pytorch/__init__.py
@@ -1,1 +1,8 @@
+import torch
+from packaging import version
+
+if version.parse(torch.__version__) >= version.parse('2.0.0'):
+    from einops._torch_specific import allow_ops_in_compiled_graph
+    allow_ops_in_compiled_graph()
+
 from block_recurrent_transformer_pytorch.block_recurrent_transformer_pytorch import BlockRecurrentTransformer, RecurrentTrainerWrapper
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'block-recurrent-transformer-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.4.0',
+  version = '0.4.1',
   license='MIT',
   description = 'Block Recurrent Transformer - Pytorch',
   author = 'Phil Wang',
@@ -19,7 +19,7 @@ setup(
   ],
   install_requires=[
     'beartype',
-    'einops>=0.4',
+    'einops>=0.6.1',
     'memorizing-transformers-pytorch>=0.4.0',
     'torch>=1.6',
   ],
